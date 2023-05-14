048b601c6f7ce8c67e2ea4a080ee413a1e8feb37
lesmemesreborn <lesmemesreborn@gmail.com>
2023-05-14 18:51:48 +0300

    added score input and styles

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,50 +1,79 @@
 <template>
   <div class="scoreboard">
     <h2>Current Score:</h2>
-    <div class="player">
-      <h3>Player: {{ currentPlayer }}</h3>
+    <div class="players"> 
+      <div class="player player1">
+        <h3>Player 1: {{ player1Score }}</h3>
+      </div>
+      <div class="player player2">
+        <h3>Player 2: {{ player2Score }}</h3>
+      </div>
     </div>
     <div class="round">
       <h3>Round: {{ currentRound }}</h3>
     </div>
-    <div class="score">
-      <h3>Score: {{ currentScore }}</h3>
-    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "Scoreboard",
   props: {
-    currentPlayer: {
-      type: String,
-      required: true,
-    },
-    currentRound: {
+    player1Score: {
       type: Number,
       required: true,
+      default: 501,
     },
-    currentScore: {
+    player2Score: {
+      type: Number,
+      required: true,
+      default: 501,
+    },
+    currentRound: {
       type: Number,
       required: true,
     },
   },
 };
 </script>
-
 <style scoped>
 .scoreboard {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 50px;
+  background-color: #f7f7f7;
+  padding: 20px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
 }
 
-.player,
-.round,
-.score {
+.players {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.player {
+  display: flex;
+  flex: 1;
   margin: 10px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+}
+
+.player:hover {
+  transform: scale(1.05);
+}
+
+.player1 {
+  border: 3px solid #bdc3c7;
+}
+
+.player2 {
+  border: 3px solid #bdc3c7;
 }
 </style>
---
+++ src/components/Scores.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="scores">
+      <label for="score-input">Введите счет:</label>
+      <div class="input-container">
+        <input type="number" id="score-input" v-model.number="score" min="0" max="180" />
+        <div class="keypad-container">
+          <button v-for="num in numbers" :key="num" @click="addNumber(num)">{{ num }}</button>
+          <button @click="addNumber(0)">0</button>
+        </div>
+      </div>
+      <button @click="submitScore">Submit</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Scores",
+    data() {
+      return {
+        score: 0,
+        numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      };
+    },
+    methods: {
+      addNumber(num) {
+        this.score = Math.min(this.score * 10 + num, 180);
+      },
+      submitScore() {
+        console.log("Submitted score:", this.score);
+        this.score = 0;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .scores {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+  }
+  
+  label {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  
+  input {
+    font-size: 36px;
+    width: 150px;
+    text-align: center;
+    padding: 10px;
+    border: 2px solid #ccc;
+    border-radius: 10px;
+    outline: none;
+    margin-right: 10px;
+    margin-bottom: 15px;
+  }
+  
+  .keypad-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 10px;
+  }
+  
+  button {
+    font-size: 28px;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 10px;
+    background-color: #ccc;
+    color: #fff;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #aaa;
+  }
+  
+  button:nth-child(10) {
+    grid-row: 4 / 5;
+    grid-column: 2 / 3;
+  }
+  
+  button:active {
+    background-color: #999;
+  }
+  </style>
